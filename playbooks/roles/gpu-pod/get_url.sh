8480159a68151946c95cb05413f44d389005d794
Jeremy Eder <jeder@redhat.com>
2018-04-30 16:48:10 -0400

    script to retrieve jupyter URL and token

---
+++ playbooks/roles/gpu-pod/get_url.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ROUTE=$(oc get routes -n nvidia | grep caffe2 | awk '{print $2}')^C
+TOKEN=$(oc logs -n nvidia pod/caffe2|head -4|grep token=|awk -Ftoken= '{print $2}')                         
+echo http://$ROUTE/notebooks/caffe2/caffe2/python/tutorials/Multi-GPU_Training.ipynb?token=$TOKEN          
